cae38e78d21c0452894b50e1f217e793865f640f
Igor Babenko <igorok.babenko@gmail.com>
2021-07-08 16:51:29 +0300

    Added comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   include ImageUploader::Attachment(:image)
 
-  has_many  :comments
+  has_many  :comments, dependent: :destroy
   validates :content, presence: true, length: {maximum: 200}
 end
