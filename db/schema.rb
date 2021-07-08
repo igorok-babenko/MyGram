c83db3557778d9d03bd001c58dca96736bfd1833
Igor Babenko <igorok.babenko@gmail.com>
2021-07-08 14:39:01 +0300

    Added column image_data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_101032) do
+ActiveRecord::Schema.define(version: 2021_07_08_113627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_08_101032) do
     t.integer "likes", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image_data"
   end
 
 end
