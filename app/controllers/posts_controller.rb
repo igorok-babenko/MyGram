caba4d29a2519ddb01bab8e79781dd79e99b83bf
Igor Babenko <igorok.babenko@gmail.com>
2021-07-09 11:59:47 +0300

    Added users_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-    before_action :load_user
+  before_action :load_user
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   #READ
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
 
-  resources :users, only: [] do
+  resources :users, only: [:show, :edit, :update] do
     resources :posts do
       resources :comments
     end
