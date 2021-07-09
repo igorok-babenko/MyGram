bfdb45bab44e2b7c317484f1971bcefae9925a92
Igor Babenko <igorok.babenko@gmail.com>
2021-07-09 11:44:59 +0300

    Changed views sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable
+
   has_many :posts
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :bio, presence: true
 end
