071233a8fc3e716e3e4f3d697a57ee1539389c66
Igor Babenko <igorok.babenko@gmail.com>
2021-07-12 17:43:30 +0300

    Added table likes in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_130542) do
+ActiveRecord::Schema.define(version: 2021_07_12_141412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 2021_07_12_130542) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "following_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -52,5 +57,7 @@ ActiveRecord::Schema.define(version: 2021_07_12_130542) do
   add_foreign_key "comments", "posts"
   add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "follows", "users", column: "following_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
