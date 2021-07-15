a476cf492776ed433a9f587292ff189a8aba260a
Igor Babenko <igorok.babenko@gmail.com>
2021-07-15 18:40:34 +0300

    Added configs to tests, created test of model Post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it { is_expected.to belong_to(:user) }
+
+  it { is_expected.to validate_presence_of(:content) }
+  it { is_expected.to validate_length_of(:content).is_at_most(200) }
+end
