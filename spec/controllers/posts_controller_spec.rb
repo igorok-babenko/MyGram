c0834eefb5081dad5e6e7e36e7c8b400914eaa46
Igor Babenko <igorok.babenko@gmail.com>
2021-07-16 18:25:12 +0300

    added test for new and create

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe PostsController, type: :controller do
 
     let!(:post) { create :post, user: user }
 
-    it 'assings @post' do
+    it 'assigns @post' do
       subject
       expect(assigns(:post)).to eq(post)
     end
@@ -35,6 +35,46 @@ RSpec.describe PostsController, type: :controller do
     context 'when user tries to see someone elses post' do
       let!(:post) { create :post }
       it { expect { subject }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+  end
+
+  describe '#new' do
+    subject { get :new, params: params }
+
+    context 'when user signed in' do
+      before {sign_in user}
+
+      it { is_expected.to render_template(:new) }
+
+      it 'assigns new post' do
+        subject
+        expect(assigns(:post)).to be_a_new Post
+      end
+    end
+  end
+
+  describe '#create' do
+    let(:params) do
+      {
+        user_id: user.id,
+        post: attributes_for(:post)
+      }
+    end
+
+    subject { post :create, params: params }
+
+    it 'create post' do
+      expect { subject }.to change { Post.count }.by(1)
+      is_expected.to redirect_to(user_post_path(assigns(:user), assigns(:post)))
+    end
+
+    context 'when params invalid' do
+      let(:params) do
+        { user_id: user.id, post: { title: nil } }
+      end
+
+      it { is_expected.to render_template :new }
+      it { expect {subject}.not_to change { Post.count } }
     end
   end
 end
