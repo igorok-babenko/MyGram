641c7d73c780ff5d6e13178cf341d4e307cd8229
Igor Babenko <igorok.babenko@gmail.com>
2021-07-18 11:46:44 +0300

    Added tests for action update in users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -46,6 +46,36 @@ RSpec.describe UsersController, type: :controller do
       expect(assigns :user).to eq user
     end
   end
+
+  describe '#update' do
+    let(:params) { { id: user, user: { name: 'changed name' } } }
+
+    subject { process :update, method: :put, params: params }
+
+    it { is_expected.to redirect_to(user_path(assigns(:user))) }
+
+    it 'updates user profile' do
+      expect { subject }.to change { user.reload.name }.to('changed name')
+    end
+
+    context 'with bad name' do
+      let(:params) { { id: user, user: { name: '' } } }
+
+      it 'does not update user profile' do
+        expect { subject }.not_to change { user.reload.name }
+      end
+    end
+
+    context 'with bad bio' do
+      let(:params) { { id: user, user: { bio: '' } } }
+
+      it 'does not update user profile' do
+        expect { subject }.not_to change { user.reload.bio }
+      end
+    end
+  end
+
+  
 end
 
 DatabaseCleaner.clean
