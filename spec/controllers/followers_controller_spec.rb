819589f014b6535a01ea8817019168b4eb17193b
Igor Babenko <igorok.babenko@gmail.com>
2021-07-19 15:43:22 +0300

    Added tests for followers_controller

---
+++ spec/controllers/followers_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'database_cleaner/active_record'
+
+DatabaseCleaner.strategy = :truncation
+
+RSpec.describe FollowersController, type: :controller do
+  let(:user) { create :user }
+  let(:params) { { user_id: user } }
+
+  before {  sign_in user }
+
+  describe '#index' do
+    subject { get :index, params: params }
+
+    it 'have not followers' do
+      subject
+      expect(assigns(:followers)).to eq([])
+    end
+
+    it { is_expected.to render_template('index') }
+  end
+end
+
+DatabaseCleaner.clean
